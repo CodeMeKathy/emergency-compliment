cf386f6ae3bd9d89adb853e87e3f203b57e685f8
Kathy Lambert <Kathy.Lambert@LotusDesignsAgency.com>
2017-12-21 18:59:01 -0500

    level2

--- index.js
+++ index.js
@@ -2,15 +2,28 @@ const express = require('express')
 const app = express()
 
 
-app.set('port', process.env.PORT || 3001)
+app.set('port', process.env.PORT || 6420)
+app.set('view engine', 'hbs')
+
 app.use('/assets', express.static('public'))
 app.get('/', (req, res) => {
     res.render('app-welcome')
 })
 
-app.set('view engine', 'hbs')
+app.get('/:name', function (request, response) {
+    var name = request.params.name
+    response.render('app-welcome', { name })
+    // response.send(`Hello ${request.params.name}`)
+
+})
 
 app.listen(app.get('port'), () => {
     console.log('It\'s aliiive!')
+})
+
+app.get('/:name', function (request, response) {
+    response.render('app-welcome',`Hello ${request.params.name}`)
+ 
+
 })
 
